faff156a2ae636993e9add2fbe81727c2c022a9b
quocbkhe150055 <107064924+kienquoc1601@users.noreply.github.com>
2024-08-22 23:49:49 +0700

    asd

---
+++ PRN231_API/PRN231_FE/Controllers/UnauthorizedController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRN231_FE.Controllers
+{
+    public class UnauthorizedController : Controller
+    {
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
